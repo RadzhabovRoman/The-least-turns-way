1bf4111e50c3af0c80db1cdcee8bc30d56ad2d86
RadzhabovRoman <38882414+RadzhabovRoman@users.noreply.github.com>
2018-05-01 18:23:36 +0500

    Update Maze.java
    Должно сработать

--- src/Maze.java
+++ src/Maze.java
@@ -40,6 +40,25 @@ public class Maze {
         }
     }
 
+    private void widthSearch() throws InterruptedException {//просто обход в ширину(чекай)
+         Queue queue = new Queue();
+         //Point[][] prev = new Point[this.prev[0].length][this.prev.length];
+         ArrayList<Point> visited = new ArrayList<>();
+         queue.enqueue(startCell);
+         while (!queue.isEmpty()){
+             Point current = (Point) queue.dequeue();
+             visited.add(current);
+             if (current.equals(finishCell))break;
+             for (Point step:direction){
+                 Point next = pointSum(current,step);
+                 if(maze[next.x][next.y]==0&&!visited.contains(next)){
+                     queue.enqueue(next);
+                     prev[next.x][next.y]=current;
+                 }
+             }
+         }
+     }
+
     private void marking() throws InterruptedException {
         sun.misc.Queue queue = new sun.misc.Queue<>();
         Point previous;
